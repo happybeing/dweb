3ce49d7d47a554346085c7f238a0419621d18378
Mark <git@happybeing.com>
2025-03-31 12:51:31 +0100

    Extend file upload APIs for private (and public) files. Document uploading from memory

--- dweb-cli/src/services/api_dweb/v0/form.rs
+++ dweb-cli/src/services/api_dweb/v0/form.rs
@@ -54,23 +54,34 @@ struct UploadForm {
 #[derive(Serialize, Deserialize, ToSchema)]
 pub struct PutResult {
     file_name: String,
+    /// The HTTP status code returned for this upload
     // #[schema(value_type = String)]
     status: u16,
     // #[schema(value_type = String, format = UInt64)]
     cost_in_attos: String,
-    // Hex encoded DataAddress
+    /// Hex encoded address of the published data map on the network. Only returned when uploading data as public
     // #[schema(value_type = String, format = Binary)]
     data_address: String,
+    /// Hex encoded data map for the uploaded data. Only returned when uploading data as private.
+    ///
+    /// This has not been stored and will be needed in order to access the data later.
+    // #[schema(value_type = String, format = Binary)]
+    data_map: String,
+    /// An explanatory message about the upload - usually only used when an error status is returned.
     // Either "success" or an explanatory error message.
     // #[schema(value_type = String)]
     message: String,
 }
 
-/// Multipart form upload of a single file
+/// Multipart form upload of a single file (as public or private)
+///
+/// Note: you can use this API to PUT data from memory instead of
+/// a file by using JavaScript. Either with a FormData object, or by
+/// setting properties on an input element.
 ///
 /// Example form:
 /// ```
-/// <form target="/" method="post" enctype="multipart/form-data">
+/// <form target="/form-upload-file/true" method="post" enctype="multipart/form-data">
 ///     <input type="file" name="file"/>
 ///     <button type="submit">Submit</button>
 /// </form>
@@ -88,29 +99,37 @@ pub struct PutResult {
 // See: https://github.com/juhaku/utoipa/discussions/742
 //    request_body(content = WhatEverStruct, description = "Multipart file", content_type = "multipart/form-data"),
     put,
-    params(("tries" = Option<u32>, Query, description = "number of times to try calling the Autonomi data_put_public() API for each file upload, 0 means unlimited. This overrides the API control setting in the server.")),
+    params(
+        ("is_public" = bool, description = "true to upload data as public"),
+        ("tries" = Option<u32>, Query, description = "number of times to try calling the Autonomi data_put_public() API for each file upload, 0 means unlimited. This overrides the API control setting in the server.")),
     request_body(content = UploadForm, content_type = "multipart/form-data"),
     responses(
         (status = 200, description = "A PutResult featuring either status 200 with cost and data address on the network, or in case of error an error status code and message about the error.<br/>\
         <b>Error StatusCodes</b><br/>\
         &nbsp;&nbsp;&nbsp;INTERNAL_SERVER_ERROR: Error reading file or storing in memory<br/>\
         &nbsp;&nbsp;&nbsp;BAD_GATEWAY: Autonomi network error", body = PutResult,
-            example = json!("{\"file_name\": \"somefile.txt\", \"status\": \"200\", \"cost_in_attos\": \"12\", \"data_address\": \"a9cd8dd0c9f2b9dc71ad548d1f37fcba6597d5eb1be0b8c63793802cc6c7de27\", \"message\": \"\" }")),
+            example = json!("{\"file_name\": \"somefile.txt\", \"status\": \"200\", \"cost_in_attos\": \"12\", \"data_address\": \"a9cd8dd0c9f2b9dc71ad548d1f37fcba6597d5eb1be0b8c63793802cc6c7de27\", \"data_map\": \"\", \"message\": \"\" }")),
     ),
     tags = ["Dweb"],
 )]
-#[put("/form-upload-file?tries={tries}")]
+#[put("/form-upload-file/{is_public}/?tries={tries}")]
 pub async fn data_put(
     MultipartForm(mut form): MultipartForm<UploadForm>,
     request: HttpRequest,
+    path_params: web::Path<bool>,
     query_params: web::Query<QueryParams>,
     client: Data<dweb::client::DwebClient>,
 ) -> HttpResponse {
+    let is_public = path_params.into_inner();
     let tries = query_params.tries.unwrap_or(client.api_control.tries);
 
     println!("DEBUG {}", request.path()); // Swagger UI execute doesn't get here, adding ;applicatation/json to the curl works
                                           // let retries = params.into_inner();
-    let put_result = put_file_public(&client, &mut form.file, tries).await;
+    let put_result = if is_public {
+        put_file_public(&client, &mut form.file, tries).await
+    } else {
+        put_file_private(&client, &mut form.file, tries).await
+    };
 
     let json = match serde_json::to_string(&put_result) {
         Ok(json) => json,
@@ -136,8 +155,98 @@ fn make_failed_put_result(file_name: String, status: StatusCode, message: String
         status: status.as_u16(),
         cost_in_attos: "0".to_string(),
         data_address: "".to_string(),
+        data_map: "".to_string(),
         message,
     }
+}
+
+#[derive(Debug, MultipartForm, ToSchema)]
+struct UploadFormList {
+    // #[multipart(rename = "file")]
+    #[multipart(limit = "100MB")] // TODO remove limit when streaming supported in Autonomi APIs
+    #[schema(value_type = Vec<String>, format = Binary)]
+    files: Vec<TempFile>,
+}
+
+#[derive(Serialize, Deserialize, ToSchema)]
+pub struct PutResultList {
+    put_results: Vec<PutResult>,
+}
+
+/// Multipart form upload of one or more files (as public or private)
+///
+/// Note: you can use this API to PUT data from memory instead of
+/// a file by using JavaScript. Either with a FormData object, or by
+/// setting properties on an input element.
+///
+/// Example form:
+/// ```
+/// <form target="/form-upload-file-list/true" method="post" enctype="multipart/form-data">
+///     <input type="file" multiple name="file"/>
+///     <button type="submit">Submit</button>
+/// </form>
+/// ```
+#[utoipa::path(
+// See: https://github.com/juhaku/utoipa/discussions/742
+//    request_body(content = WhatEverStruct, description = "Multipart file", content_type = "multipart/form-data"),
+    put,
+    params(
+        ("is_public" = bool, description = "true to upload data as public"),
+        ("tries" = Option<u32>, Query, description = "number of times to try calling the Autonomi data_put_public() API for each file upload, 0 means unlimited. This overrides the API control setting in the server.")),
+    request_body(content = UploadFormList, content_type = "multipart/form-data"),
+    responses(
+        (status = 200, description = "A PutResultList featuring a PutResult for each upload either status 200 with cost and data address on the network, or in case of error an error status code and message about the error.<br/>\
+        <b>Error StatusCodes</b><br/>\
+        &nbsp;&nbsp;&nbsp;INTERNAL_SERVER_ERROR: Error reading file or storing in memory<br/>\
+        &nbsp;&nbsp;&nbsp;BAD_GATEWAY: Autonomi network error", body = [PutResultList],
+            example = json!("{\"put_results\": [{\"file_name\": \"somefile.txt\", \"status\": \"200\", \"cost_in_attos\": \"12\", \"data_address\": \"a9cd8dd0c9f2b9dc71ad548d1f37fcba6597d5eb1be0b8c63793802cc6c7de27\", \"data_map\": \"\", \"message\": \"\" }]}")),
+    ),
+    tags = ["Dweb"],
+)]
+#[put("/form-upload-file-list/{is_public}/?tries={tries}")]
+pub async fn data_put_list(
+    MultipartForm(form): MultipartForm<UploadFormList>,
+    request: HttpRequest,
+    path_params: web::Path<bool>,
+    query_params: web::Query<QueryParams>,
+    client: Data<dweb::client::DwebClient>,
+) -> HttpResponse {
+    println!("DEBUG {}", request.path());
+    let is_public = path_params.into_inner();
+    let tries = query_params.tries.unwrap_or(client.api_control.tries);
+
+    let mut put_list = PutResultList {
+        put_results: Vec::<PutResult>::new(),
+    };
+    for mut file in form.files {
+        println!(
+            "DEBUG data_put_list() file: {:?}, size: {}",
+            file.file_name, file.size
+        );
+        let put_result = if is_public {
+            put_file_public(&client, &mut file, tries).await
+        } else {
+            put_file_private(&client, &mut file, tries).await
+        };
+        put_list.put_results.push(put_result);
+    }
+
+    let json = match serde_json::to_string(&put_list) {
+        Ok(json) => json,
+        Err(e) => {
+            return make_error_response_page(
+                Some(StatusCode::INTERNAL_SERVER_ERROR),
+                &mut HttpResponse::NotFound(),
+                "/data PUT error".to_string(),
+                &format!("data_put_public() failed to encode JSON result - {e}"),
+            )
+        }
+    };
+
+    println!("DEBUG put_result as JSON: {json:?}");
+    HttpResponse::Ok()
+        .insert_header(ContentType(mime::APPLICATION_JSON))
+        .body(json)
 }
 
 async fn put_file_public(client: &DwebClient, file: &mut TempFile, tries: u32) -> PutResult {
@@ -148,7 +257,7 @@ async fn put_file_public(client: &DwebClient, file: &mut TempFile, tries: u32) -
         Ok(content_len) => content_len,
         Err(e) => {
             let message =
-                format!("data_put_public() failed to read file '{file_name}' into buffer - {e}");
+                format!("put_file_public() failed to read file '{file_name}' into buffer - {e}");
             println!("DEBUG {message}");
             return make_failed_put_result(file_name, StatusCode::INTERNAL_SERVER_ERROR, message);
         }
@@ -173,97 +282,73 @@ async fn put_file_public(client: &DwebClient, file: &mut TempFile, tries: u32) -
 
     match result {
         Ok(result) => {
-            println!("DEBUG data_put_public() stored '{file_name}' {content_len} bytes on the network at address");
+            println!("DEBUG put_file_public() stored '{file_name}' {content_len} bytes on the network at address");
             PutResult {
                 file_name,
                 status: StatusCode::OK.as_u16(),
                 cost_in_attos: format_tokens_as_attos(result.0.as_atto()),
                 data_address: result.1.to_hex(),
+                data_map: "".to_string(),
                 message: "success".to_string(),
             }
         }
         Err(e) => {
             let message =
-                format!("data_put_public() failed store file '{file_name}' on the network - {e}");
+                format!("put_file_public() failed store file '{file_name}' on the network - {e}");
             println!("DEBUG {message}");
             return make_failed_put_result(file_name, StatusCode::BAD_GATEWAY, message);
         }
     }
 }
 
-#[derive(Debug, MultipartForm, ToSchema)]
-struct UploadFormList {
-    // #[multipart(rename = "file")]
-    #[multipart(limit = "100MB")] // TODO remove limit when streaming supported in Autonomi APIs
-    #[schema(value_type = Vec<String>, format = Binary)]
-    files: Vec<TempFile>,
-}
-
-#[derive(Serialize, Deserialize, ToSchema)]
-pub struct PutResultList {
-    put_results: Vec<PutResult>,
-}
-
-/// Multipart form upload of one or more files
-///
-/// Example form:
-/// ```
-/// <form target="/" method="post" enctype="multipart/form-data">
-///     <input type="file" multiple name="file"/>
-///     <button type="submit">Submit</button>
-/// </form>
-/// ```
-#[utoipa::path(
-// See: https://github.com/juhaku/utoipa/discussions/742
-//    request_body(content = WhatEverStruct, description = "Multipart file", content_type = "multipart/form-data"),
-    put,
-    params(("tries" = Option<u32>, Query, description = "number of times to try calling the Autonomi data_put_public() API for each file upload, 0 means unlimited. This overrides the API control setting in the server.")),
-    request_body(content = UploadFormList, content_type = "multipart/form-data"),
-    responses(
-        (status = 200, description = "A PutResultList featuring a PutResult for each upload either status 200 with cost and data address on the network, or in case of error an error status code and message about the error.<br/>\
-        <b>Error StatusCodes</b><br/>\
-        &nbsp;&nbsp;&nbsp;INTERNAL_SERVER_ERROR: Error reading file or storing in memory<br/>\
-        &nbsp;&nbsp;&nbsp;BAD_GATEWAY: Autonomi network error", body = [PutResultList],
-            example = json!("{\"put_results\": [{\"file_name\": \"somefile.txt\", \"status\": \"200\", \"cost_in_attos\": \"12\", \"data_address\": \"a9cd8dd0c9f2b9dc71ad548d1f37fcba6597d5eb1be0b8c63793802cc6c7de27\", \"message\": \"\" }]}")),
-    ),
-    tags = ["Dweb"],
-)]
-#[put("/form-upload-file-list")]
-pub async fn data_put_list(
-    MultipartForm(form): MultipartForm<UploadFormList>,
-    request: HttpRequest,
-    query_params: web::Query<QueryParams>,
-    client: Data<dweb::client::DwebClient>,
-) -> HttpResponse {
-    println!("DEBUG {}", request.path());
-    let tries = query_params.tries.unwrap_or(client.api_control.tries);
-
-    let mut put_list = PutResultList {
-        put_results: Vec::<PutResult>::new(),
-    };
-    for mut file in form.files {
-        println!(
-            "DEBUG data_put_list() file: {:?}, size: {}",
-            file.file_name, file.size
-        );
-        let put_result = put_file_public(&client, &mut file, tries).await;
-        put_list.put_results.push(put_result);
-    }
-
-    let json = match serde_json::to_string(&put_list) {
-        Ok(json) => json,
+async fn put_file_private(client: &DwebClient, file: &mut TempFile, tries: u32) -> PutResult {
+    // TODO update if Autonomi supports streamed data_put() (or file_put_public())
+    let mut content = Vec::<u8>::new();
+    let file_name = file.file_name.clone().unwrap_or("unknown".to_string());
+    let content_len = match file.file.read_to_end(&mut content) {
+        Ok(content_len) => content_len,
         Err(e) => {
-            return make_error_response_page(
-                Some(StatusCode::INTERNAL_SERVER_ERROR),
-                &mut HttpResponse::NotFound(),
-                "/data PUT error".to_string(),
-                &format!("data_put_public() failed to encode JSON result - {e}"),
-            )
+            let message =
+                format!("put_file_private() failed to read file '{file_name}' into buffer - {e}");
+            println!("DEBUG {message}");
+            return make_failed_put_result(file_name, StatusCode::INTERNAL_SERVER_ERROR, message);
         }
     };
 
-    println!("DEBUG put_result as JSON: {json:?}");
-    HttpResponse::Ok()
-        .insert_header(ContentType(mime::APPLICATION_JSON))
-        .body(json)
+    let data = content.into();
+    let payment_option = client.payment_option().clone();
+    let result = retry_until_ok(
+        tries,
+        &"data_put()",
+        (data, payment_option),
+        async move |(data, payment_option)| match client
+            .client
+            .data_put(data, payment_option.clone())
+            .await
+        {
+            Ok(result) => Ok(result),
+            Err(e) => Err(eyre!(e)),
+        },
+    )
+    .await;
+
+    match result {
+        Ok(result) => {
+            println!("DEBUG put_file_private() stored '{file_name}' {content_len} bytes on the network at address");
+            PutResult {
+                file_name,
+                status: StatusCode::OK.as_u16(),
+                cost_in_attos: format_tokens_as_attos(result.0.as_atto()),
+                data_address: "".to_string(),
+                data_map: result.1.to_hex(),
+                message: "success".to_string(),
+            }
+        }
+        Err(e) => {
+            let message =
+                format!("put_file_private() failed store file '{file_name}' on the network - {e}");
+            println!("DEBUG {message}");
+            return make_failed_put_result(file_name, StatusCode::BAD_GATEWAY, message);
+        }
+    }
 }
